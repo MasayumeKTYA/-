7885803d10e655b498f45bfc047421265c189831
= <1419965049@qq.com>
2023-11-25 21:38:22 +0800

    第一次

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <view>详情</view>
+
+    <Audio />
+  
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Audio from "@/component/play.vue";
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="head">
+    <image class="avatar" src="/static/img/avatar.jpg" lazy-load />
+    <view style="height: 60rpx"></view>
+    <view class="conatin">
+      <view class="username">Masayume</view>
+    </view>
+  </view>
+  <view class="title">歌单</view>
+  <view class="box">
+    <view class="box_data" v-for="item in [0, 1, 2, 3, 4]" :key="item">
+      <view class="left"></view>
+      <view class="right">
+        <view class="sing_title">标题标题标题</view>
+        <view class="sing_num">10首</view>
+      </view>
+    </view>
+  </view>
+  <Audio />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Audio from "@/component/play.vue";
+</script>
+
+<style>
+@import "./index.css";
+</style>
